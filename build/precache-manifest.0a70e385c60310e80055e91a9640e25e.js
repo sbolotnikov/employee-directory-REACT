12bb9f42aebf1ebcb9d84c9ee5c165b328b7ba8c
Sergey Bolotnikov <sbolotnikov@gmail.com>
2020-11-26 18:34:11 -0500

    deployment

---
+++ build/precache-manifest.0a70e385c60310e80055e91a9640e25e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30e5e0cd556922ddefc3161259e110d0",
+    "url": "/employee-directory-react/index.html"
+  },
+  {
+    "revision": "b10491a9e9a5a0e02768",
+    "url": "/employee-directory-react/static/css/main.d687bf5e.chunk.css"
+  },
+  {
+    "revision": "88add2773e5029a76ec2",
+    "url": "/employee-directory-react/static/js/2.d27a0aad.chunk.js"
+  },
+  {
+    "revision": "c53f65871ab74006a0c4e0b92b224267",
+    "url": "/employee-directory-react/static/js/2.d27a0aad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b10491a9e9a5a0e02768",
+    "url": "/employee-directory-react/static/js/main.ef3e5f6c.chunk.js"
+  },
+  {
+    "revision": "7c483b2024677cc109e7",
+    "url": "/employee-directory-react/static/js/runtime-main.0757c0fd.js"
+  }
+]);
